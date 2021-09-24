2dc44d5bb1c5a437416076161726b1d58f77f3a2
Peter LaPan <peter.lapan@labsolns.com>
2021-09-24 16:36:21 -0400

    tags working

--- bookmunger.scm
+++ bookmunger.scm
@@ -14,37 +14,72 @@
 	     (ice-9 i18n)   ;; internationalization
 	     (ice-9 popen)
 	     (ice-9 regex) ;;list-matches
-	     (ice-9 receive)	     
+	     (ice-9 receive)
+	     (ice-9 format)
 	     (ice-9 string-fun)  ;;string-replace-substring
 	     (ice-9 pretty-print)
+	     (ice-9 textual-ports)
+	     (ice-9 ftw) ;; file tree walk
+	     (ice-9 readline)
 	     (bookmunger utilities)
 	     (bookmunger logging)   ;; logging is in guile-lib
              (logging logger)
              (logging rotating-log)
              (logging port-log)
+	     (sxml simple)
 	     )
 
 (define book-count 0)
-(define all-chars "-a-zA-Z0-9ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁłŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽžſǍǎǏǐǑǒǓǔǕǖǗǘǙǚǛǜƏƒƠơƯƯǺǻǼǽǾǿńŻć<>~_+=,.:;()&#@®\" ")
+
+(define lib-dir "/home/mbc/temp/lib/") ;; home of library XML
+(define lib-backup-dir "/home/mbc/temp/lib/backups/") ;;
+(define on-deck-dir "/home/mbc/temp/lib/")  ;; out of z-lib ready to have z-lib removed
+(define no-zlib-dir "/home/temp/mbc/lib/nozlib/")  ;; no z-lib, ready for title author extraction; regular books here
+(define dest-dir "/home/temp/lib/mbc/finalmod/") ;; final destination directory probably ~/syncd/library/files
+(define lib-file-name "Library1.reflib")
+
+
+(define tags '((0 . fiction)	(1 . nonfiction)(2 . technical)(3 . R)(4 . statistics)(5 . Bayes)(6 . popgen)(7 . gametheory)(9 . bitcoin)(10 . genetics)(11 . work)(12 . admixture)(13 . DOE)(14 . manuals)(15 . programming)(16 . math)(17 . smalltalk)(18 . history)(19 . philosophy)))
+
 
 (define (remove-zlib str)
   (let* ((dot (string-rindex str #\.)) ;;reverse search
 	 (name (substring str 0  dot ))
 	 (len (length (string->list name)))
-	 (a (substring name (- len 11) len))
-	 (is-it-zlib? (string= " (zlib.org)" a)))
+	 (a (substring name (- len 12) len))
+	 (is-it-zlib? (string= " (z-lib.org)" a)))
     (if is-it-zlib?
 	(let* ((len2 (length (string->list str)))
 	       (ext (substring str dot len2))  ;;inlcudes the .
-	       (new-name (substring name 0 (- len 11))))
+	       (new-name (substring name 0 (- len 12))))
 	  (string-append new-name ext))
 	str)))
 
 
-;;(remove-zlib "some book name by Peter LaPan (zlib.org).epub")
+;;(remove-zlib "Gazelle by Ducornet Rikki (z-lib.org).epub")
+
+(define (rm-zlib-from-filename fname)
+  (let* ((pref on-deck-dir)
+	 (old (string-append pref fname))
+	 (new-pre (remove-zlib fname))
+	 (new (string-append pref new-pre)))
+ ;;   (pretty-print new-pre)))
+  (rename-file old new )))
+
+;;(rm-zlib-from-filename "The First Man by Camus, Albert (z-lib.org).epub")
+
+(define (recurse-rm-zlib-from-filename lst)
+  (if (null? (cdr lst))
+      (rm-zlib-from-filename (car lst))
+      (begin
+	(rm-zlib-from-filename (car lst))
+	(recurse-rm-zlib-from-filename (cdr lst)))))
+
+  
+;; (recurse-rm-zlib-from-filename  (cddr (scandir "/home/mbc/Downloads/lib")))
 
 
-(define (get-title-author str)
+(define (get-title-author-filename str)
   ;; return a list '(title author new-file-name)
   ;; if "by" is in the title, I will not extract author
   (let* ((len (length (string->list str)))
@@ -61,11 +96,105 @@
 	 (new-file-name (string-append title suf))
 	 )
 
-  new-file-name ))
+  `(,title ,author ,new-file-name) ))
 
-(get-title-author "some book the name by Peter LaPan (zlib.org).epub")
+;;(get-title-author-filename "some book the name by Peter LaPan (zlib.org).epub")
 
 
+
+(define myfile (open-file (string-append lib-dir lib-file-name) "r"))
+
+(define a (xml->sxml myfile))
+
+;; ((*PI* xml "version=\"1.0\" encoding=\"UTF-8\"")
+;; (library...
+;;  (manage_target...)
+;;  (library_folder...)
+;;  (taglist...
+;;     (tag)
+;;     (tag))
+;;  (doclist...
+;;     (doc)
+;;     (doc)))))
+
+;;get each element of the xml
+;; I want to add to the doclist
+
+
+
+(define pi (cadr a));; (*PI* xml "version=\"1.0\" encoding=\"UTF-8\"")
+;;(pretty-print (cadr a))
+(define manage-target (cadr (cdaddr a));; (library "\n\t" (manage_target (@ (utf8 "false") (braces...
+;;(pretty-print (cadr (cdaddr a)))
+(define library-folder (cdddr a));;
+;;(pretty-print (cadddr (cdaddr a)))
+(define taglist (caddr (cddddr (caddr a))));;
+;;(pretty-print (caddr (cddddr (caddr a))))
+(define doclist (car (cddddr (cddddr (caddr a)))))
+;;(pretty-print (car (cddddr (cddddr (caddr a)))))
+;;print library
+;;(pretty-print  (caddr a))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define lib (cddr a)) ;;library
+(define doclist (car (cddddr (cddddr lib)))) ;;doclist
+(pretty-print doclist)
+(caddr doclist)
+
+(define (make-tags arg)
+  ;; expect tags to be string of numbers "11 2 5 7"
+  (let* ((tags (string-split arg #\space ))
+	 
+
+	 )
+    ))
+
+(define out (open-file "/home/mbc/projects/bookmunger/out.txt" "w"))
+
+(format out "~a" "some text" )
+
+(define myvar 11)
+
+(begin (format out "~a" "(tagged \"" "\")" )
+(force-output out))
+
+
+(string-split "4 5 6 7 8" #\space)
+
+(define adoc `(doc "\n\t\t\t" (relative_filename "files/1-s2.0-0022175995002626-main.pdf") "\n\t\t\t" (key "1_s2.0_0022175") "\n\t\t\t" (notes) "\n\t\t\t" (tagged "11") "\n\t\t\t" (bib_type "article") "\n\t\t\t" (bib_doi) "\n\t\t\t" (bib_title "Application of experimental design techniques to optimize a competitive ELISA.") "\n\t\t\t" (bib_authors) "\n\t\t\t" (bib_journal) "\n\t\t\t" (bib_volume) "\n\t\t\t" (bib_number) "\n\t\t\t" (bib_pages) "\n\t\t\t" (bib_year "1996") "\n\t\t"))
+
+(pretty-print adoc)
+
+
+
+(define (recurse-make-tag lst new-lst)
+  (if (null? (cdr lst))
+      (begin
+	 (set! new-lst (cons (string-append "(tagged \"" (car lst) "\"") new-lst))
+	 new-lst)
+      (begin
+	(set! new-lst (cons (string-append "(tagged \"" (car lst) "\"") new-lst))
+	(recurse-make-tag (cdr lst) new-lst)))
+  )
+
+;;(pretty-print (recurse-make-tag (string-split "4 5 6 7 8" #\space) '()))
+
+(define (make-tags args)
+  ;;args is a string of integers i.e. "4 5 6 7 8"
+  (let*((a (string-split args #\space))
+	(b (recurse-make-tag a '())))
+    b))
+
+
+(define (make-doc filename key tags title authors  )
+   `(doc "\n\t\t\t" (relative_filename ,(string-append "files/" filename)) "\n\t\t\t" (key "1_s2.0_0022175") "\n\t\t\t" (notes) "\n\t\t\t" ,@tags "\n\t\t\t" (bib_type "book") "\n\t\t\t" (bib_doi) "\n\t\t\t" (bib_title ,title) "\n\t\t\t" (bib_authors ,authors) "\n\t\t\t" (bib_journal) "\n\t\t\t" (bib_volume) "\n\t\t\t" (bib_number) "\n\t\t\t" (bib_pages) "\n\t\t\t" (bib_year) "\n\t\t")
+  )
+
+
+
+(pretty-print (make-doc "myfile" "mykey" (make-tags "11 2 5 6") "A Title" "P.B Woodhouse, Joe Schmoe"))
 
 
 (define (main args)
