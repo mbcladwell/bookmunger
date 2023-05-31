4606c5860f8cc0ec3c8078fff1f14d3cb7dee65e
mbcladwell <mbcladwell@gmail.com>
2023-05-31 16:16:08 -0400

    autocommit

--- manifest.scm
+++
@@ -1,3 +0,0 @@
-(specifications->manifest
-		'("guile@3.0.7" "guile-lib" "coreutils" "gawk" "sed" "findutils" "glibc@2.33" "grep" "bash-minimal" "openssl" "gnutls" "curl" "glibc-locales" "glibc-utf8-locales" "guile-readline" "guile-dbi"))
-
